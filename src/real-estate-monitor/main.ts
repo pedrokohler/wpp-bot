7838e5b10ff10cf759d3e8e4589d3a2f13c18113
Pedro H. Köhler <pedrokohlerbh@gmail.com>
2023-09-21 01:17:35 +0000

    Delete src/real-estate-monitor/main.ts

--- src/real-estate-monitor/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RealEstateMonitorModule } from './monitor.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(RealEstateMonitorModule);
-  await app.listen(3000);
-}
-bootstrap();
