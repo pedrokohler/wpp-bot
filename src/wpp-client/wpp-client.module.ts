ca24b2038e926cd7f2d989b871a4f1f6a5c934c8
Pedro Kohler <pedrokohlerbh@gmail.com>
2023-09-27 20:59:58 -0300

    feat: add auto-restart when client is down

--- src/wpp-client/wpp-client.module.ts
+++ src/wpp-client/wpp-client.module.ts
@@ -3,9 +3,10 @@ import { WppClientService } from './wpp-client.service';
 import { CommandModule } from 'src/commands/command.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [CommandModule, ConfigModule, MongooseModule],
+  imports: [CommandModule, ConfigModule, MongooseModule, EmailModule],
   exports: [WppClientService],
   providers: [WppClientService],
 })
--- src/wpp-client/wpp-client.service.ts
+++ src/wpp-client/wpp-client.service.ts
@@ -6,37 +6,87 @@ import {
   RemoteAuth,
   WAState,
 } from 'whatsapp-web.js';
-import { CommandService } from 'src/commands/command.service';
+import { CommandService, ME } from 'src/commands/command.service';
 import { ConfigService } from '@nestjs/config';
 import mongoose, { Connection } from 'mongoose';
 import { InjectConnection } from '@nestjs/mongoose';
 import { MongoStore } from 'wwebjs-mongo';
+import { EmailService } from 'src/email/email.service';
 
 @Injectable()
 export class WppClientService {
   private readonly logger = new Logger(WppClientService.name);
-  public readonly client: Client;
+  public client: Client;
+  public intervalRef;
   constructor(
     private configService: ConfigService,
     private commandService: CommandService,
+    private emailService: EmailService,
     @InjectConnection() private connection: Connection,
   ) {
+    this.initializeClient();
+  }
+
+  private async handleMessageCreate(message: Message) {
+    await this.commandService.runMessageCreatedTriggers(message, this.client);
+    await this.commandService.runCommand(message, this.client);
+  }
+
+  public async sendMessage({ chatId, content }) {
+    await this.client.sendMessage(chatId, content, { sendSeen: false });
+  }
+
+  public async onModuleDestroy() {
+    this.logger.log('Destroying client.');
+    await this.client.destroy();
+    this.logger.log('Client destroyed.');
+  }
+
+  private async isClientOnline() {
+    try {
+      const contactId = ME;
+      const contact = await this.client.getContactById(contactId);
+
+      if (contact.isMe && contact.isUser) {
+        this.logger.log('The client is running fine...');
+      }
+    } catch (error) {
+      clearInterval(this.intervalRef);
+      this.logger.error(`The client is down. Killing process...`);
+      process.exit(1);
+    }
+  }
+
+  // Export a function to start the polling interval.
+  private startPolling() {
+    clearInterval(this.intervalRef);
+    const intervalInMilliseconds = 5000; // 20 minutes i.e. 20 minutes * 60 seconds * 1000 milliseconds
+    this.intervalRef = setInterval(() => {
+      this.isClientOnline();
+    }, intervalInMilliseconds);
+  }
+
+  private initializeClient() {
     this.logger.log(
-      `PUPPETEER_EXECUTABLE_PATH: ${configService.get<string>(
+      `PUPPETEER_EXECUTABLE_PATH: ${this.configService.get<string>(
         'PUPPETEER_EXECUTABLE_PATH',
       )}`,
     );
-    const store = new MongoStore({ mongoose: { ...mongoose, connection } });
+    const store = new MongoStore({
+      mongoose: { ...mongoose, connection: this.connection },
+    });
     this.client = new Client({
       authStrategy:
-        configService.get<string>('NODE_ENV') !== 'production'
+        this.configService.get<string>('NODE_ENV') !== 'production'
           ? new LocalAuth()
           : new RemoteAuth({
               store,
               backupSyncIntervalMs: 300 * 1000,
             }),
       puppeteer: {
-        executablePath: configService.get<string>('PUPPETEER_EXECUTABLE_PATH'),
+        executablePath: this.configService.get<string>(
+          'PUPPETEER_EXECUTABLE_PATH',
+        ),
         args: ['--no-sandbox'],
         // headless: false,
       },
@@ -53,6 +103,7 @@ export class WppClientService {
     });
 
     this.client.on('ready', () => {
+      this.startPolling();
       this.logger.log('Client is ready!');
       this.commandService.notifyBotStatus({
         client: this.client,
@@ -76,30 +127,27 @@ export class WppClientService {
 
     this.client.on('disconnected', (reason) => {
       this.logger.error('Disconnected', reason);
-      this.commandService.notifyBotStatus({
-        client: this.client,
-        status: WAState.UNPAIRED,
-        message: `Bot disconnected ${new Date().toTimeString()}.`,
-      });
+      this.emailService
+        .sendEmail({
+          title: 'Bot disconnected',
+          body: `Your bot was disconnected from whatsapp because of ${reason}`,
+        })
+        .catch((e) => this.logger.error(`Failed to send email: ${e.message}`));
+      this.commandService
+        .notifyBotStatus({
+          client: this.client,
+          status: WAState.UNPAIRED,
+          message: `Bot disconnected ${new Date().toTimeString()}.`,
+        })
+        .catch((e) =>
+          this.logger.error(
+            `Failed to send whatsapp notification: ${e.message}`,
+          ),
+        );
     });
 
     this.client.on('message_create', this.handleMessageCreate.bind(this));
 
     this.client.initialize();
-  }
-
-  private async handleMessageCreate(message: Message) {
-    await this.commandService.runMessageCreatedTriggers(message, this.client);
-    await this.commandService.runCommand(message, this.client);
-  }
-
-  public async sendMessage({ chatId, content }) {
-    await this.client.sendMessage(chatId, content, { sendSeen: false });
-  }
-
-  public async onModuleDestroy() {
-    this.logger.log('Destroying client.');
-    await this.client.destroy();
-    this.logger.log('Client destroyed.');
   }
 }
